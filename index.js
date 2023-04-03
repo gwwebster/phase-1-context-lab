16e4c0bb721a576360c08545f2ed4a6a54622b6b
George <gwwebster23@gmail.com>
2023-04-03 18:04:30 -0500

    Context lab

--- index.js
+++ index.js
@@ -1,13 +1,76 @@
-/* Your Code Here */
+function createEmployeeRecord(employeeArray) {
+    let employeeObj = {
+        firstName: employeeArray[0],
+        familyName: employeeArray[1],
+        title: employeeArray[2],
+        payPerHour: employeeArray[3],
+        timeInEvents: [],
+        timeOutEvents: [],
+    }
+    return employeeObj
+};
 
-/*
- We're giving you this function. Take a look at it, you might see some usage
- that's new and different. That's because we're avoiding a well-known, but
- sneaky bug that we'll cover in the next few lessons!
+function createEmployeeRecords(arrayOfArrays) {
+    let arrayOfEmployeeObjs = []
+    arrayOfArrays.forEach(employeeArray => {
+        arrayOfEmployeeObjs.push(createEmployeeRecord(employeeArray))
+    })
+    return arrayOfEmployeeObjs
+};
 
- As a result, the lessons for this function will pass *and* it will be available
- for you to use if you need it!
- */
+function createTimeInEvent(date) {
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: Number(date.slice(11)),
+        date: date.slice(0, 10),
+    })
+    return this
+};
+
+function createTimeOutEvent(date) {
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: Number(date.slice(11)),
+        date: date.slice(0, 10),
+    })
+    return this
+};
+
+function hoursWorkedOnDate(searchDate) {
+    let timeIn
+    let timeOut
+    this.timeInEvents.forEach(timeInObj => {
+        if (timeInObj.date === searchDate) {
+            timeIn = timeInObj.hour
+        }
+    })
+    this.timeOutEvents.forEach(timeOutObj => {
+        if (timeOutObj.date === searchDate) {
+            timeOut = timeOutObj.hour
+        }
+    })
+    return (timeOut - timeIn)/100
+};
+
+function wagesEarnedOnDate(date) {
+    return (hoursWorkedOnDate.call(this, date) * this.payPerHour)
+};
+
+function findEmployeeByFirstName(srcArray, searchFirstName) {
+    let foundEmployee
+    srcArray.forEach(employeeObj => {
+        if (employeeObj.firstName === searchFirstName) {
+            foundEmployee = employeeObj
+        }
+    })
+    return foundEmployee
+};
+
+function calculatePayroll(arrayOfEmployeeObjs) {
+    return arrayOfEmployeeObjs.reduce((accumulator, initialValue) => {
+        return accumulator + allWagesFor.call(initialValue)
+    }, 0)
+};
 
 const allWagesFor = function () {
     const eligibleDates = this.timeInEvents.map(function (e) {
@@ -16,8 +79,8 @@ const allWagesFor = function () {
 
     const payable = eligibleDates.reduce(function (memo, d) {
         return memo + wagesEarnedOnDate.call(this, d)
-    }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
+    }.bind(this), 0)
 
     return payable
-}
+};
 
